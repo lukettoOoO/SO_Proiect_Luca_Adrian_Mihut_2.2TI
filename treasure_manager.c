5b2e2852da5e4eabf3ec58dc76312138b195a812
lukettoOoO <110674888+lukettoOoO@users.noreply.github.com>
2025-04-08 18:54:56 +0300

    finished phase 1

--- treasure_manager.c
+++ treasure_manager.c
@@ -89,6 +89,79 @@ void add_hunt(char *hunt_id)
     }
 }
 
+void add_treasure(char *hunt_id, char *id, char *username, char *latitude, char *longitude, char *clue, char *value) 
+{
+    char path[128];
+    memset(path, 0, sizeof(path));
+    strcat(path, "./");
+    strcat(path, hunt_id);
+    path[strlen(path)] = '\0';
+
+    char treasure_file[128];
+    memset(treasure_file, 0, sizeof(treasure_file));
+    strcat(treasure_file, path);
+    strcat(treasure_file, "/treasure.dat");
+    treasure_file[strlen(treasure_file)] = '\0';
+    int treasure_fd = open(treasure_file, O_WRONLY | O_APPEND, 0666);
+    if (treasure_fd == -1) 
+    {
+        perror("(add_treasure - open treasure.dat)");
+        exit(EXIT_FAILURE);
+    }
+
+    Treasure treasure;
+    treasure.id = atoi(id);
+    strcpy(treasure.username, username);
+    treasure.latitude = atof(latitude);
+    treasure.longitude = atof(longitude);
+    strcpy(treasure.clue, clue);
+    treasure.value = atoi(value);
+
+    if (write(treasure_fd, &treasure, sizeof(treasure)) == -1) 
+    {
+        perror("(add_treasure - write)");
+        exit(EXIT_FAILURE);
+    }
+    close(treasure_fd);
+
+    char log_file[128];
+    memset(log_file, 0, sizeof(log_file));
+    strcat(log_file, path);
+    strcat(log_file, "/logged_hunt.log");
+    log_file[strlen(log_file)] = '\0';
+    int log_fd = open(log_file, O_WRONLY | O_APPEND, 0666);
+    if (log_fd == -1) 
+    {
+        perror("(add_treasure - open logged_hunt.log)");
+        exit(EXIT_FAILURE);
+    }
+
+    char log_msg[256];
+    memset(log_msg, 0, sizeof(log_msg));
+    strcat(log_msg, "Added treasure to hunt ");
+    strcat(log_msg, hunt_id);
+    strcat(log_msg, " - id=");
+    strcat(log_msg, id);
+    strcat(log_msg, ", username='");
+    strcat(log_msg, username);
+    strcat(log_msg, "', latitude=");
+    strcat(log_msg, latitude);
+    strcat(log_msg, ", longitude=");
+    strcat(log_msg, longitude);
+    strcat(log_msg, ", clue='");
+    strcat(log_msg, clue);
+    strcat(log_msg, "', value=");
+    strcat(log_msg, value);
+    strcat(log_msg, "\n");
+    log_msg[strlen(log_msg)] = '\0';
+    if (write(log_fd, log_msg, strlen(log_msg)) == -1) 
+    {
+        perror("(add_treasure - write)");
+        exit(EXIT_FAILURE);
+    }
+    close(log_fd);
+}
+
 void list(char *hunt_id)
 {
     char path[128];
